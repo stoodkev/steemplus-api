864a656c5813b08b4600e106756a4c041ff0a4dc
Stoodkev <quentin.correa@gmail.com>
2018-04-16 15:15:06 +0800

    Change in Witness API

--- routes/routes.js
+++ routes/routes.js
@@ -32,10 +32,10 @@ app.get("/api/get-witness/:username", function(req, res){
     console.log("connected");
     return pool.request()
     .input("username",req.params.username)
-    .query('SELECT B.timestamp as lastBlockTimestamp, rankedTable.*\
-    FROM (SELECT ROW_NUMBER() OVER (ORDER BY (SELECT votes) DESC) AS rank, * FROM Witnesses (NOLOCK) WHERE signing_key != \'STM1111111111111111111111111111111114T1Anm\') AS rankedTable, Blocks B\
-    WHERE B.block_num = rankedTable.last_confirmed_block_num\
-    AND name = @username')
+    .query('SELECT timestamp, Witnesses.* \
+    FROM Witnesses (NOLOCK) \
+    LEFT JOIN Blocks ON Witnesses.last_confirmed_block_num = Blocks.block_num \
+    WHERE Witnesses.name = @username')
   }).then(result => {
     res.status(200).send(result.recordsets[0][0]);
     sql.close();
