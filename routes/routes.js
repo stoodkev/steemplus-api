71336c7989eeec8c9ec3fbfc5a66e443d2810660
Stoodkev <quentin.correa@gmail.com>
2018-04-16 11:12:57 +0800

    Adding get-witnnesses-rank

--- routes/routes.js
+++ routes/routes.js
@@ -43,6 +43,21 @@ app.get("/api/get-witness/:username", function(req, res){
   sql.close();});
 });
 
+app.get("/api/get-witnesses-rank", function(req, res){
+  console.log(config.config_api);
+  sql.connect(config.config_api).then(pool => {
+    console.log("connected");
+    return pool.request()
+    .query('Select Witnesses.name, rank\
+  from Witnesses (NOLOCK)\
+  LEFT JOIN (SELECT ROW_NUMBER() OVER (ORDER BY (SELECT votes) DESC) AS rank, * FROM Witnesses (NOLOCK) WHERE signing_key != 'STM1111111111111111111111111111111114T1Anm') AS rankedTable ON Witnesses.name = rankedTable.name;')
+  }).then(result => {
+    res.status(200).send(result.recordsets[0][0]);
+    sql.close();
+  }).catch(error => {console.log(error);
+  sql.close();});
+});
+
 }
 
 module.exports = appRouter;
