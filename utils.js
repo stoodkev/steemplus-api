ebf8fde929e997af259fb017a118f5ce381a77c4
Stoodkev <quentin.correa@gmail.com>
2018-05-09 10:49:34 +0800

    Text change

--- utils.js
+++ utils.js
@@ -1,7 +1,8 @@
 exports.commentNewUser=function(post,lastUpdate,numUsers) {
-  let commentBody = `#### Welcome to Steem, @${post.author}!\n`;
-  commentBody += `I am a bot coded by the SteemPlus team to help you make the best of your experience on Steem!\n`;
+  let commentBody = `#### Welcome to Steem, @${post.author}!\n\n`;
+  commentBody += `I am a bot coded by the SteemPlus team to help you make the best of your experience on the Steem Blockchain!\n`;
   commentBody += `SteemPlus is a Chrome, Opera and Firefox extension that adds tons of features on Steemit.\n`;
-  commentBody += `To see why **${numUsers} Steemians** use SteemPlus, [install our extension](https://chrome.google.com/webstore/detail/steemplus/mjbkjgcplmaneajhcbegoffkedeankaj?hl=en), read the [documentation](https://github.com/stoodkev/SteemPlus/blob/master/README.md) or the latest release : [${lastUpdate.title}](${lastUpdate.url})\n`;
+  commentBody += `It helps you see the real value of your account, who mentionned you, the value of the votes received, a filtered and sorted feed and much more! All of this in a fast and secure way.\n`
+  commentBody += `To see why **${numUsers} Steemians** use SteemPlus, [install our extension](https://chrome.google.com/webstore/detail/steemplus/mjbkjgcplmaneajhcbegoffkedeankaj?hl=en), read the [documentation](https://github.com/stoodkev/SteemPlus/blob/master/README.md) or the latest release : [${lastUpdate.title}](${lastUpdate.url}).\n`;
   return commentBody;
 }
