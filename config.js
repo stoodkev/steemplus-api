43355cee4efbacd0290cec2c83d4126cf9936894
Cedric Guillas <cedric.guillas78@gmail.com>
2018-09-04 16:40:53 +0800

    Updated config file for MongoDB

--- config.js
+++ config.js
@@ -5,10 +5,19 @@ const config_api = {
     password: process.env.PASSWORD,
     server: process.env.SQL_API,
     database: process.env.DB,
+    requestTimeout: 60000
+}
+
+const local_db_config = {
+  user: process.env.LOCAL_LOGIN,
+  password: process.env.LOCAL_PASSWORD,
+  server: process.env.LOCAL_MONGO_API,
+  database: process.env.LOCAL_DB
 }
 
 let config={
   config_api:config_api,
+  local_db_config:local_db_config,
   bot:process.env.BOT,
   wif:process.env.WIF,
   key:process.env.MASTER,
