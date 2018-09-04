6725560cdd14e884c3bdabb5b2ff124bb2b8ae6f
Cedric Guillas <cedric.guillas78@gmail.com>
2018-09-04 16:42:11 +0800

    Updated app.js for MongoDB

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var routes = require("./routes/routes.js");
 var app = express();
 require('dotenv').config();
 var RateLimit = require('express-rate-limit');
+var mongoose = require ("mongoose");
+
+var TypeTransactionCollection = "TypeTransaction";
+var UserCollection = "User";
+var PointDetailCollection = "PointDetail";
+
 
 app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
 
@@ -12,14 +18,24 @@ var limiter = new RateLimit({
   max: 20, // limit each IP to 20 requests per windowMs
   delayMs: 0 // disable delaying - full speed until the max limit is reached
 });
+
+
 //  apply to all requests
 app.use(limiter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-routes(app);
-
-var server = app.listen(process.env.PORT||3000, function () {
-    console.log("app running on port ", server.address().port);
+var uristring = process.env.MONGOLAB_PINK_URI || process.env.MONGODB_URI||'mongodb://127.0.0.1:27017/heroku_fl6ldd26';
+mongoose.connect(uristring, function (err, res) {
+  if (err) 
+  {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else 
+  {
+    var server = app.listen(process.env.PORT||3000, function () {
+      console.log("app running on port ", server.address().port);
+    });
+    console.log ('Succeeded connected to: ' + uristring);
+  }
 });
+routes(app);
