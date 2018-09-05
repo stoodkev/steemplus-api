36bc9e62465d4114fd9594e159ff289e537dfc9e
Cedric Guillas <cedric.guillas78@gmail.com>
2018-09-05 18:05:06 +0800

    Added function to create comments body

--- utils.js
+++ utils.js
@@ -11,3 +11,20 @@ exports.formatDate = function(string)
 {
 	return `${string.getFullYear()}-${string.getMonth()+1}-${string.getDate()} ${string.getHours()}:${string.getMinutes()}:${string.getSeconds()}.${string.getMilliseconds()}`;
 }
+
+exports.commentVotingBot=function(post)
+{
+	return commentBody = `
+	<div>
+		<p>Hi, ${post.author}!</p>
+		<p>You just got a <b>${post.percentage}%</b> upvote by SteemPlus!</p>
+		<p>You can get higher rewards by using SteemPlus even more! Click <a target="_blank" href="https://steemit.com/utopian-io/@steem-plus/steemplus-points-calculation">here</a> to get more details about how to earn more <b>SteemPlus Points</b>.</p>
+		<p>You can also join us on Discord <a target= "_blank" href="https://discord.gg/7zX46q">https://discord.gg/7zX46q</a>.</p>
+		<p> If you like SteemPlus, don't forget to vote for @stoodkev as a witness. Only need to click <a target="_blank" href="https://v2.steemconnect.com/sign/account-witness-vote?witness=stoodkev&amp;approve=1">here</a>!</p>
+	</div>`;
+}
+
+exports.commentVotingBotTest=function(post)
+{
+	return commentBody = 'Test';
+}
