a0d07d3880f40c2eda509d3983e77fee2ebb23ac
Cedric Guillas <cedric.guillas78@gmail.com>
2018-09-06 23:32:13 +0800

    Updated query to get steemplus points from posts

--- routes/routes.js
+++ routes/routes.js
@@ -358,14 +358,14 @@ var appRouter = function (app) {
       if(lastEntry[0] !== undefined)
         lastEntryDate = lastEntry[0].timestampString;
       else
-        lastEntryDate = '2018-08-03 12:05:42.000'; // This date is the steemplus point annoncement day
+        lastEntryDate = '2018-08-10 12:05:42.000'; // This date is the steemplus point annoncement day + 7 days for rewards because rewards come after 7 days.
       // Wait for SteemSQL's query result before starting the second request
       // We decided to wait to be sure this function won't try to update the same row twice at the same time
       await new sql.ConnectionPool(config.config_api).connect().then(pool => {
         return pool.request()
         .query(`
           SELECT
-            REPLACE(VOCommentBenefactorRewards.reward, ' VESTS', '') as reward, Comments.created, Comments.author, Comments.title, Comments.url, Comments.permlink, Comments.beneficiaries, Comments.total_payout_value
+            REPLACE(VOCommentBenefactorRewards.reward, ' VESTS', '') as reward, VOCommentBenefactorRewards.timestamp as created , Comments.author, Comments.title, Comments.url, Comments.permlink, Comments.beneficiaries, Comments.total_payout_value
           FROM
             VOCommentBenefactorRewards
             INNER JOIN Comments ON VOCommentBenefactorRewards.author = Comments.author AND VOCommentBenefactorRewards.permlink = Comments.permlink
