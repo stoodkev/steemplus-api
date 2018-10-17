226fa4e55af7c997d0e7acb2bfa48d8ea2250b95
Cedric Guillas <cedric.guillas78@gmail.com>
2018-10-17 17:12:10 +0800

    Added dtube to benefactors

--- routes/routes.js
+++ routes/routes.js
@@ -1260,7 +1260,7 @@ async function updateSteemplusPointsComments(comments)
 
     // Get type
     var type = 'default';
-    if(comment.beneficiaries.includes('dtube.rewards'))
+    if(comment.beneficiaries.includes('dtube.rewards')||comment.beneficiaries.includes('dtube'))
       type = await TypeTransaction.findOne({name: 'DTube'});
     else if(comment.beneficiaries.includes('utopian.pay'))
       type = await TypeTransaction.findOne({name: 'Utopian.io'});
