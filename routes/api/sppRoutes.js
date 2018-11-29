be3b8cf7ee87ed2fae37e55659934fe0ac022f62
Cedric Guillas <cedric.guillas78@gmail.com>
2018-11-29 15:45:16 +0800

    Added endPoint to get a list of premium features

--- routes/api/sppRoutes.js
+++ routes/api/sppRoutes.js
@@ -1,4 +1,5 @@
 const spp = require("../../controllers/api/spp.js");
+const premium = require("../../controllers/api/premium.js");
 
 const sppRouter = function(app) {
   // Function used to get the details of an account.
@@ -6,6 +7,10 @@ const sppRouter = function(app) {
   // Return the number of points of an account and other information as the detail of every entry of steemplus point
   app.get("/spp/:username", async function(req, res) {
     res.status(200).send(await spp.getSPP(req.params.username));
+  });
+
+  app.get("/premium-feature-list", async function(req, res) {
+	res.status(200).send(await premium.getFeatureList());
   });
 };
 
