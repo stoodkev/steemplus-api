23324b618d8b611974700eb60ce4db5a9f4d0fe6
Cedric Guillas <cedric.guillas78@gmail.com>
2018-11-30 11:43:32 +0800

    Added description to get feature list query

--- controllers/api/premium.js
+++ controllers/api/premium.js
@@ -2,7 +2,7 @@
 const PremiumFeature = require("../../models/premiumFeature.js");
 
 exports.getFeatureList = async function() {
-	return await PremiumFeature.find({}, {'name':1, 'price':1, '_id':0}, function (err, docs) {
+	return await PremiumFeature.find({}, {'name':1, 'price':1, 'description':1, '_id':0}, function (err, docs) {
 		return docs;
 	});
 }
