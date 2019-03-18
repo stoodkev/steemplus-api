eebe31b5ee092c5f8d5de91fefbaebb799b2965d
Stoodkev <quentin.correa@gmail.com>
2019-03-18 10:38:49 +0800

    Add routes for getting and adding Ads

---
+++ routes/api/adsRoutes.js
@@ -0,0 +1,20 @@
+const ads = require("../../controllers/api/ads.js");
+
+const apiRouter = function(app) {
+  // Get all the ads that have been paid for and did not reach payout yet.
+  // @parameters: none
+  app.get("/ads", async function(req, res) {
+    res.status(200).send(await ads.getAds());
+  });
+
+  app.post("/ads", async function(req, res) {
+    if (req.body.key !== config.key) {
+      res.status(403).send("Permission denied");
+      return;
+    }
+    else if(req.body.url)
+      res.status(200).send(await ads.createAd(req.body.url));
+  });
+};
+
+module.exports = apiRouter;
